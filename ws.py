f8d08377338d69657ad30c6330987bcf384186c8
Felipe Lamim <fclamim@gmail.com>
2018-04-04 16:14:14 -0300

    Permitir que o usuário escolha a operação

--- ws.py
+++ ws.py
@@ -1,9 +1,22 @@
-from suds.client import Client
-a = int(input("Digite o primeiro número: "))
-b = int(input("Digite o segundo número: "))
-print(Client('http://localhost:10002/calculadora?wsdl').service.Somar(a, b))
-print(Client('http://localhost:10002/calculadora?wsdl').service.Substrair(a, b))
-print(Client('http://localhost:10002/calculadora?wsdl').service.Multiplicar(a, b))
-print(Client('http://localhost:10002/calculadora?wsdl').service.Dividir(a, b))
+﻿from suds.client import Client
+a = float(input("Digite o primeiro número: "))
+b = float(input("Digite o segundo número: "))
+op = int(input("Escolha a operação - 1) Soma 2) Subtração"))
 
+if op == 1:
+    print(Client('http://localhost:10002/calculadora?wsdl').service.Somar(a, b))
+
+elif op == 2:
+    print(Client('http://localhost:10002/calculadora?wsdl').service.Substrair(a, b))
+
+elif op == 3:
+    print(Client('http://localhost:10002/calculadora?wsdl').service.Multiplicar(a, b))
+
+elif op == 4:
+    print(Client('http://localhost:10002/calculadora?wsdl').service.Dividir(a, b))
+
+else:
+    result = print("Operação inválida!")
+
+print(result)
 
